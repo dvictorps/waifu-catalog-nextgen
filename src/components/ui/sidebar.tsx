513cb057566fd79cba286e2c011b06a592d71edb
Victor <dvictorps@gmail.com>
2025-09-07 16:31:46 -0300

    Enhance Sidebar component with navigation links and Home button integration

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,7 +1,10 @@
 "use client";
 
+import { HomeIcon } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import { Separator } from "../separator";
+import { Button } from "./button";
 
 export function Sidebar() {
 	return (
@@ -15,6 +18,26 @@ export function Sidebar() {
 				/>
 			</div>
 			<Separator className="bg-purple-900" />
+			<ul>
+				<li>
+					<Link href="/">
+						<Button
+							variant="ghost"
+							className="flex w-full justify-start text-white hover:bg-purple-900"
+						>
+							<span className="flex flex-row gap-2">
+								<HomeIcon /> Home
+							</span>
+						</Button>
+					</Link>
+				</li>
+				<li>
+					<Link href="/">Home</Link>
+				</li>
+				<li>
+					<Link href="/">Home</Link>
+				</li>
+			</ul>
 		</aside>
 	);
 }
