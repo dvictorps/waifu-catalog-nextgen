aa7502097dcc934ad05fc1084530d699ca469757
Victor <dvictorps@gmail.com>
2025-09-10 21:10:39 -0300

    Update layout styles to ensure main content area has a minimum height for better display. Modify waifu type definition to access data correctly from the API response structure.

--- src/types/waifu.ts
+++ src/types/waifu.ts
@@ -2,4 +2,4 @@ import type { inferRouterOutputs } from "@trpc/server";
 import type { appRouter } from "~/server/api/root";
 
 export type InferRouteOutputs = inferRouterOutputs<typeof appRouter>;
-export type Waifu = InferRouteOutputs["waifu"]["all"][number];
+export type Waifu = InferRouteOutputs["waifu"]["all"]["data"][number];
